a57f68aff1d0c7d8ba21cddebf9f48750c344f69
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-11-14 14:35:02 +0900

    Setting: project 초기세팅 완료

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import * as Joi from 'joi';
+
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV == 'dev' ? '.env' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        DB: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '../**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
